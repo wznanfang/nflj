d1031ff8c1c9d5d926c2deb00c23cf4bee597359
wzpkuye <1348069215@qq.com>
2021-05-13 14:01:23 +0800

    fix: 修改相关

--- src/main/java/com/wzp/nflj/security/WebSecurityConfig.java
+++ src/main/java/com/wzp/nflj/security/WebSecurityConfig.java
@@ -53,7 +53,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService());
-//        auth.inMemoryAuthentication().withUser("wzp").password(new BCryptPasswordEncoder().encode("123456")).roles("admin"); //将用户写在内存里
     }
 
 
