7099b47854fa4d944c8544c5b28443468a6d7bb7
1348069215@qq.com <951130wzx+>
2021-05-27 13:42:56 +0800

    fix: 修改相关配置

--- src/main/java/com/wzp/nflj/security/AuthorizationServerConfig.java
+++ src/main/java/com/wzp/nflj/security/AuthorizationServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
