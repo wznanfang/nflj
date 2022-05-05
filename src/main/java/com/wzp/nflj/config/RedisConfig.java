cdcb18bd0e4f283f193fe4ab2ef928c7606231fb
wznanfang <wzp9215@qq.com>
2022-05-05 10:02:14 +0800

    fix: 修改redis

--- src/main/java/com/wzp/nflj/config/RedisConfig.java
+++ src/main/java/com/wzp/nflj/config/RedisConfig.java
@@ -19,27 +19,20 @@ import java.util.Map;
 import java.util.Set;
 
 /**
+ * 采用 JdkSerializationRedisSerializer 进行序列化和反序列化
+ *
  * @Author: zp.wei
  * @DATE: 2020/10/13 13:57
  */
 @Configuration
-@AutoConfigureAfter(RedisAutoConfiguration.class)  // 在加载配置的类之后再加载当前类
 public class RedisConfig {
 
 
     /**
-     * 该方式采用 jackson2JsonRedisSerializer 进行序列化和反序列化
+     * 该方式采用 JdkSerializationRedisSerializer 进行序列化和反序列化
      */
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory factory) {
-        //使用Jackson会有缓存转换异常问题
-        /*Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        //解决查询缓存转换异常的问题
-        ObjectMapper om = new ObjectMapper();
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.WRAPPER_ARRAY);
-        jackson2JsonRedisSerializer.setObjectMapper(om);*/
-
         RedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
         //配置序列化(解决乱码的问题)
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
@@ -48,15 +41,12 @@ public class RedisConfig {
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(jdkSerializationRedisSerializer))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jdkSerializationRedisSerializer))
                 .disableCachingNullValues();
-
         //设置缓存空间
         Set<String> cacheNames = new HashSet<>();
         cacheNames.add("nflj");
-
         // 对每个缓存空间应用不同的配置
         Map<String, RedisCacheConfiguration> configMap = new HashMap<>();
         configMap.put("nflj", config);
-
         RedisCacheManager cacheManager = RedisCacheManager
                 .builder(factory)
                 .cacheDefaults(config)
